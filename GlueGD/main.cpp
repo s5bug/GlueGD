b2feafd7ffbd511f5faf9c0a010dfcc934469983
Aly Cerruti <me@s5.pm>
2021-08-18 15:24:28 -0700

    It has been 2021 for 8 months

--- GlueGD/main.cpp
+++ GlueGD/main.cpp
@@ -1,5 +1,5 @@
 /* GlueGD - A library that manages Geometry Dash mods
- * Copyright (C) 2020 Aly Cerruti
+ * Copyright (C) 2021 Aly Cerruti
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- GlueGDLoader/main.cpp
+++ GlueGDLoader/main.cpp
@@ -1,5 +1,5 @@
 /* GlueGDLoader - A fake XInput DLL that loads GlueGD
- * Copyright (C) 2020 Aly Cerruti
+ * Copyright (C) 2021 Aly Cerruti
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
