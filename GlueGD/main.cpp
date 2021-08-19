e49aaaaae4a9517caee012470fcb48743e8d3509
Aly Cerruti <me@s5.pm>
2021-08-18 18:15:55 -0700

    Send inputs to ImGui

--- GlueGD/main.cpp
+++ GlueGD/main.cpp
@@ -18,6 +18,7 @@
 #include <cstdio>
 #include <cstdint>
 #include <cinttypes>
+#include <queue>
 #include <span>
 
 #define WIN32_LEAN_AND_MEAN
@@ -40,6 +41,9 @@ HMODULE libcocos2d = NULL;
 
 using CocosSwapBuffersWrapperProc = void (*)(GLFWwindow*);
 CocosSwapBuffersWrapperProc cocosSwapBuffersWrapper = NULL;
+
+using CocosPollEventsWrapperProc = void (*)();
+CocosPollEventsWrapperProc cocosPollEventsWrapper = NULL;
 
 HDC adaptGlfwWindowHdc(GLFWwindow* window) {
     uintptr_t windowAddr = reinterpret_cast<uintptr_t>(window);
@@ -75,9 +79,123 @@ void __fastcall cocosSwapBuffersHook(cocos2d::CCEGLView* thisx, void* edx) {
     return;
 }
 
-void __fastcall cocosRunWithSceneHook(cocos2d::CCDirector* thisx, void* edx, cocos2d::CCScene* pScene) {
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+void __fastcall cocosPollEventsHook(cocos2d::CCEGLView* thisx, void* edx) {
     (void) edx;
+    (void) thisx; // GLFW context is global
 
+    ImGuiIO& io = ImGui::GetIO();
+
+    bool doReplace;
+    MSG msg;
+    while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+        TranslateMessage(&msg);
+
+        doReplace = true;
+
+        if(io.WantCaptureMouse) {
+            switch(msg.message) {
+                case WM_CAPTURECHANGED:
+                case WM_LBUTTONDBLCLK:
+                case WM_LBUTTONDOWN:
+                case WM_LBUTTONUP:
+                case WM_MBUTTONDBLCLK:
+                case WM_MBUTTONDOWN:
+                case WM_MBUTTONUP:
+                case WM_MOUSEACTIVATE:
+                case WM_MOUSEHOVER:
+                case WM_MOUSEHWHEEL:
+                case WM_MOUSELEAVE:
+                case WM_MOUSEMOVE:
+                case WM_MOUSEWHEEL:
+                case WM_NCHITTEST:
+                case WM_NCLBUTTONDBLCLK:
+                case WM_NCLBUTTONDOWN:
+                case WM_NCLBUTTONUP:
+                case WM_NCMBUTTONDBLCLK:
+                case WM_NCMBUTTONDOWN:
+                case WM_NCMBUTTONUP:
+                case WM_NCMOUSEHOVER:
+                case WM_NCMOUSELEAVE:
+                case WM_NCMOUSEMOVE:
+                case WM_NCRBUTTONDBLCLK:
+                case WM_NCRBUTTONDOWN:
+                case WM_NCRBUTTONUP:
+                case WM_NCXBUTTONDBLCLK:
+                case WM_NCXBUTTONDOWN:
+                case WM_NCXBUTTONUP:
+                case WM_RBUTTONDBLCLK:
+                case WM_RBUTTONDOWN:
+                case WM_RBUTTONUP:
+                case WM_XBUTTONDBLCLK:
+                case WM_XBUTTONDOWN:
+                case WM_XBUTTONUP:
+                    doReplace = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if(io.WantCaptureKeyboard) {
+            switch(msg.message) {
+                case WM_ACTIVATE:
+                case WM_APPCOMMAND:
+                case WM_CHAR:
+                case WM_DEADCHAR:
+                case WM_HOTKEY:
+                case WM_KEYDOWN:
+                case WM_KEYUP:
+                case WM_KILLFOCUS:
+                case WM_SETFOCUS:
+                case WM_SYSDEADCHAR:
+                case WM_SYSKEYDOWN:
+                case WM_SYSKEYUP:
+                case WM_UNICHAR:
+                    doReplace = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if(doReplace) {
+            DispatchMessage(&msg);
+        }
+
+        ImGui_ImplWin32_WndProcHandler(msg.hwnd, msg.message, msg.wParam, msg.lParam);
+    }
+
+    cocosPollEventsWrapper();
+}
+
+void patchEventPolling() {
+    // cocos2d::CCEGLView::pollEvents is just a wrapper around glfwPollEvents
+    FARPROC pollEventsProc = GetProcAddress(libcocos2d, "?pollEvents@CCEGLView@cocos2d@@QAEXXZ");
+    uintptr_t pollEventsAddr = reinterpret_cast<uintptr_t>(pollEventsProc);
+    uint8_t* pollEventsData = reinterpret_cast<uint8_t*>(pollEventsAddr);
+
+    if(pollEventsData[0x00] == 0xE9) {
+        uintptr_t cocosPollEventsWrapperOffset;
+        std::memcpy(&cocosPollEventsWrapperOffset, pollEventsData + 1, sizeof(uintptr_t));
+        cocosPollEventsWrapper = reinterpret_cast<CocosPollEventsWrapperProc>(pollEventsAddr + cocosPollEventsWrapperOffset + 0x05);
+
+        uintptr_t cocosPollEventsHookAddr = reinterpret_cast<uintptr_t>(&cocosPollEventsHook);
+        std::vector<uint8_t> newCode = { 0xE9, 0x00, 0x00, 0x00, 0x00 };
+        uintptr_t offset = cocosPollEventsHookAddr - (pollEventsAddr + newCode.size());
+        std::memcpy(newCode.data() + 1, &offset, sizeof(offset));
+
+        DWORD oldProtections;
+        VirtualProtect(pollEventsProc, 0x5, PAGE_EXECUTE_READWRITE, &oldProtections);
+        std::memcpy(pollEventsProc, newCode.data(), newCode.size());
+        VirtualProtect(pollEventsProc, 0x5, oldProtections, &oldProtections);
+    } else {
+        printf("Something is wrong with CCEGLView::pollEvents! Is your GD installation modified?\n");
+        printf("Not installing GUI event hook. GUI will be nonfunctional...\n");
+    }
+}
+
+void patchSwapBuffers(cocos2d::CCDirector* thisx) {
     // We know for certain that we're not rendering a scene right now, so we can patch the method
     // We also know that libcocos2d is non-null because it had to be non-null for us to get here
     FARPROC swapBuffersProc = GetProcAddress(libcocos2d, "?swapBuffers@CCEGLView@cocos2d@@UAEXXZ");
@@ -116,9 +234,17 @@ void __fastcall cocosRunWithSceneHook(cocos2d::CCDirector* thisx, void* edx, coc
         HWND hwnd = WindowFromDC(hDC);
         ImGui_ImplWin32_Init(hwnd);
         ImGui_ImplOpenGL3_Init();
+
+        patchEventPolling();
     } else {
         printf("Couldn't locate glfwSwapBuffers. Not hooking GUI...\n");
     }
+}
+
+void __fastcall cocosRunWithSceneHook(cocos2d::CCDirector* thisx, void* edx, cocos2d::CCScene* pScene) {
+    (void) edx;
+
+    patchSwapBuffers(thisx);
 
     // Then let's run the original cocos2d code
     thisx->pushScene(pScene);
